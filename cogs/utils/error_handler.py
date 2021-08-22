feaac6b4d6486b43075d88cccf45fb40a8309f77
Ay355 <77558710+Ay-355@users.noreply.github.com>
2021-08-21 22:24:42 -0700

    Fix error handler

--- cogs/utils/error_handler.py
+++ cogs/utils/error_handler.py
@@ -1,21 +1,22 @@
 import discord
 from discord.ext import commands
+import traceback
 
 
 async def error_handler(ctx, error):
 
     if isinstance(error, commands.NoPrivateMessage):
-        await ctx.author.send("Bot can't be used in private messages")
+        return await ctx.author.send("Bot can't be used in private messages")
 
     if isinstance(error, commands.MissingPermissions):
-        await ctx.send("You do not have the required permissions needed to do that")
+        return await ctx.send("You do not have the required permissions needed to do that")
 
     if isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send("Hmm, looks like you are missing a required argument. "
-                        "Please do `,help command` for more info")
+        return await ctx.send(f"Hmm, looks like you are missing a required argument. "
+                        "Please do `{ctx.prefix}help command` for more info")
 
     if isinstance(error, commands.BotMissingPermissions):
-        await ctx.send("Looks like I don't have the required permissions to do this. "
+        return await ctx.send("Looks like I don't have the required permissions to do this. "
                         "Please make sure I have the permissions needed")
 
     if isinstance(error, commands.CommandNotFound):
@@ -32,4 +33,6 @@ async def error_handler(ctx, error):
 
     if isinstance(error, commands.CommandError):
         return
+
+    return traceback.print_exc()
 
